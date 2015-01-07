b5deb5f11420de993d8455a2e6f116faac5c7a0f
William Suárez <suarezwilliamf@boolinc.co>
2015-01-07 18:05:50 -0500

    commit inicial

---
+++ app.js
@@ -0,0 +1,16 @@
+module.exports = function (sum){
+
+  var q = app.utils.q;
+  var promise = require('promise');
+
+
+  var sum = function (num1, num2, res){
+
+    if ( this.num1 + this.num2 == this.res){
+    console.log('fullfield');
+
+    }else{
+    new Error("el resultado no corresponde con la suma de los números");
+    }
+
+};
